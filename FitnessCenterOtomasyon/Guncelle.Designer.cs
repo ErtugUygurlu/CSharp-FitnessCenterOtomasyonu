acb8d331a293e4c3ff1e0c4ae60634d227932cae
ErtugUygurlu <ertu_ygrl@hotmail.com>
2022-12-02 21:06:23 +0300

    FitnessCenterOtomasyon
    FitnessCenterOtomasyonu(c#/sql)

---
+++ FitnessCenterOtomasyon/Guncelle.Designer.cs
@@ -0,0 +1,362 @@
+﻿namespace FitnessCenterOtomasyonu
+{
+    partial class Guncelle
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button2 = new System.Windows.Forms.Button();
+            this.ZamanlamaCb = new System.Windows.Forms.ComboBox();
+            this.CinsiyetCb = new System.Windows.Forms.ComboBox();
+            this.YasTb = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.OdemeTb = new System.Windows.Forms.TextBox();
+            this.TelefonTb = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.AdSoyadTb = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.UyeDGV = new System.Windows.Forms.DataGridView();
+            this.label10 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.UyeDGV)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Firebrick;
+            this.button2.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button2.ForeColor = System.Drawing.Color.White;
+            this.button2.Location = new System.Drawing.Point(12, 513);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(107, 52);
+            this.button2.TabIndex = 22;
+            this.button2.Text = "Güncelle";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // ZamanlamaCb
+            // 
+            this.ZamanlamaCb.BackColor = System.Drawing.Color.Black;
+            this.ZamanlamaCb.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.ZamanlamaCb.ForeColor = System.Drawing.Color.White;
+            this.ZamanlamaCb.FormattingEnabled = true;
+            this.ZamanlamaCb.Items.AddRange(new object[] {
+            "06:00-08:00",
+            "08:00-10:00",
+            "18:00-20:00",
+            "20:00-22:00"});
+            this.ZamanlamaCb.Location = new System.Drawing.Point(17, 461);
+            this.ZamanlamaCb.Name = "ZamanlamaCb";
+            this.ZamanlamaCb.Size = new System.Drawing.Size(229, 31);
+            this.ZamanlamaCb.TabIndex = 20;
+            // 
+            // CinsiyetCb
+            // 
+            this.CinsiyetCb.BackColor = System.Drawing.Color.Black;
+            this.CinsiyetCb.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.CinsiyetCb.ForeColor = System.Drawing.Color.White;
+            this.CinsiyetCb.FormattingEnabled = true;
+            this.CinsiyetCb.Items.AddRange(new object[] {
+            "Bay",
+            "Bayan"});
+            this.CinsiyetCb.Location = new System.Drawing.Point(17, 250);
+            this.CinsiyetCb.Name = "CinsiyetCb";
+            this.CinsiyetCb.Size = new System.Drawing.Size(229, 31);
+            this.CinsiyetCb.TabIndex = 19;
+            // 
+            // YasTb
+            // 
+            this.YasTb.BackColor = System.Drawing.Color.Black;
+            this.YasTb.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.YasTb.ForeColor = System.Drawing.Color.White;
+            this.YasTb.Location = new System.Drawing.Point(17, 317);
+            this.YasTb.Multiline = true;
+            this.YasTb.Name = "YasTb";
+            this.YasTb.Size = new System.Drawing.Size(229, 36);
+            this.YasTb.TabIndex = 17;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Century Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label6.ForeColor = System.Drawing.Color.Red;
+            this.label6.Location = new System.Drawing.Point(12, 284);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(55, 30);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Yaş";
+            // 
+            // OdemeTb
+            // 
+            this.OdemeTb.BackColor = System.Drawing.Color.Black;
+            this.OdemeTb.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.OdemeTb.ForeColor = System.Drawing.Color.White;
+            this.OdemeTb.Location = new System.Drawing.Point(17, 389);
+            this.OdemeTb.Multiline = true;
+            this.OdemeTb.Name = "OdemeTb";
+            this.OdemeTb.Size = new System.Drawing.Size(229, 36);
+            this.OdemeTb.TabIndex = 18;
+            // 
+            // TelefonTb
+            // 
+            this.TelefonTb.BackColor = System.Drawing.Color.Black;
+            this.TelefonTb.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.TelefonTb.ForeColor = System.Drawing.Color.White;
+            this.TelefonTb.Location = new System.Drawing.Point(17, 178);
+            this.TelefonTb.Multiline = true;
+            this.TelefonTb.Name = "TelefonTb";
+            this.TelefonTb.Size = new System.Drawing.Size(229, 36);
+            this.TelefonTb.TabIndex = 16;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Century Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label8.ForeColor = System.Drawing.Color.Red;
+            this.label8.Location = new System.Drawing.Point(12, 356);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(131, 30);
+            this.label8.TabIndex = 13;
+            this.label8.Text = "Aylık Tutar";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Century Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.ForeColor = System.Drawing.Color.Red;
+            this.label5.Location = new System.Drawing.Point(12, 145);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(220, 30);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Telefon Numarası";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Century Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label9.ForeColor = System.Drawing.Color.Red;
+            this.label9.Location = new System.Drawing.Point(12, 428);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(158, 30);
+            this.label9.TabIndex = 10;
+            this.label9.Text = "Zamanlama";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Century Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label7.ForeColor = System.Drawing.Color.Red;
+            this.label7.Location = new System.Drawing.Point(12, 217);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(107, 30);
+            this.label7.TabIndex = 9;
+            this.label7.Text = "Cinsiyet";
+            // 
+            // AdSoyadTb
+            // 
+            this.AdSoyadTb.BackColor = System.Drawing.Color.Black;
+            this.AdSoyadTb.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.AdSoyadTb.ForeColor = System.Drawing.Color.White;
+            this.AdSoyadTb.Location = new System.Drawing.Point(17, 106);
+            this.AdSoyadTb.Multiline = true;
+            this.AdSoyadTb.Name = "AdSoyadTb";
+            this.AdSoyadTb.Size = new System.Drawing.Size(229, 36);
+            this.AdSoyadTb.TabIndex = 15;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Century Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.ForeColor = System.Drawing.Color.Red;
+            this.label4.Location = new System.Drawing.Point(12, 73);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(190, 30);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Üye Adı Soyadı";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Century Gothic", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.ForeColor = System.Drawing.Color.Blue;
+            this.label2.Location = new System.Drawing.Point(369, 53);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(310, 28);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Üye Güncelleme ve Silme";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Century Gothic", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.ForeColor = System.Drawing.Color.Red;
+            this.label3.Location = new System.Drawing.Point(968, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(44, 44);
+            this.label3.TabIndex = 14;
+            this.label3.Text = "X";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Century Gothic", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(381, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(288, 44);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "FITNESS CENTER";
+            // 
+            // UyeDGV
+            // 
+            this.UyeDGV.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.UyeDGV.Location = new System.Drawing.Point(324, 106);
+            this.UyeDGV.Name = "UyeDGV";
+            this.UyeDGV.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.UyeDGV.Size = new System.Drawing.Size(678, 386);
+            this.UyeDGV.TabIndex = 23;
+            this.UyeDGV.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label10.ForeColor = System.Drawing.Color.Firebrick;
+            this.label10.Location = new System.Drawing.Point(830, 85);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(172, 16);
+            this.label10.TabIndex = 7;
+            this.label10.Text = "Silinecek Üyeye Tıklayınız";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Firebrick;
+            this.button1.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(125, 513);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(107, 52);
+            this.button1.TabIndex = 22;
+            this.button1.Text = "Sil";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Firebrick;
+            this.button3.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button3.ForeColor = System.Drawing.Color.White;
+            this.button3.Location = new System.Drawing.Point(238, 513);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(107, 52);
+            this.button3.TabIndex = 22;
+            this.button3.Text = "Reset";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.Firebrick;
+            this.button4.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button4.ForeColor = System.Drawing.Color.White;
+            this.button4.Location = new System.Drawing.Point(351, 513);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(107, 52);
+            this.button4.TabIndex = 22;
+            this.button4.Text = "Geri";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Guncelle
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(1025, 577);
+            this.Controls.Add(this.UyeDGV);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.ZamanlamaCb);
+            this.Controls.Add(this.CinsiyetCb);
+            this.Controls.Add(this.YasTb);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.OdemeTb);
+            this.Controls.Add(this.TelefonTb);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.AdSoyadTb);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Guncelle";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Guncelle";
+            this.Load += new System.EventHandler(this.Guncelle_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.UyeDGV)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ComboBox ZamanlamaCb;
+        private System.Windows.Forms.ComboBox CinsiyetCb;
+        private System.Windows.Forms.TextBox YasTb;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox OdemeTb;
+        private System.Windows.Forms.TextBox TelefonTb;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox AdSoyadTb;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView UyeDGV;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+    }
+}
